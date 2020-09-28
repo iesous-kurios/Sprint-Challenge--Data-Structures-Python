153326f717663fb4a02fb5f6b18d16738e278fdd
iesous-kurios <j.wylie.81@gmail.com>
2020-09-27 18:53:26 -0700

    imported bst and completed mvp of names.py

--- names/names.py
+++ names/names.py
@@ -1,5 +1,7 @@
 import time
 
+from binary_search_tree import BinarySearchTree
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,14 +15,29 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+bst = BinarySearchTree()
+
+# Insert names into instance of bst
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bst.insert(name_1)
+
+# find and append duplicates
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+"""
+Pre-optimized code:
+  Time complexity: O(n^2)
+  Run-time: 4.928653 seconds
+
+Optimized code using a binary search tree:
+  Run-time: 0.05270957946777344 seconds"""
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
