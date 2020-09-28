95a362813b643a1db948c0daf06e0f350bca8c1c
iesous-kurios <j.wylie.81@gmail.com>
2020-09-27 18:44:14 -0700

    finished ring buffer class

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,40 @@
 class RingBuffer:
+    """
+    RingBuffer Class
+
+    Parameters:
+
+    capacity : int
+    size : int
+    write_index : int
+    storage : list
+
+    Methods:
+     - append
+         adds the given element to the buffer
+
+     - get
+          returns all the elements in the buffer in a list
+          in their given order
+    """
+
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.size = 0
+        self.write_index = 0
+        self.storage = []
 
     def append(self, item):
-        pass
+        if self.size >= self.capacity:
+            self.storage[self.write_index] = item
+        else:
+            self.storage.append(item)
+            self.size += 1
+
+        if self.write_index < (self.capacity - 1):
+            self.write_index += 1
+        else:
+            self.write_index = 0
 
     def get(self):
-        pass
+        return self.storage
